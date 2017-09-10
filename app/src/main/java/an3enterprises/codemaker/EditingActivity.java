d81c682c1048da56832927cadf4d2bc1a043186c
andres <8bitandres>
2017-09-10 12:36:45 -0700

    Changing lots of stuff.

--- app/src/main/java/an3enterprises/codemaker/EditingActivity.java
+++ app/src/main/java/an3enterprises/codemaker/EditingActivity.java
@@ -1,20 +1,14 @@
 package an3enterprises.codemaker;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.KeyEvent;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class EditingActivity extends AppCompatActivity {
 
@@ -39,6 +33,7 @@ public class EditingActivity extends AppCompatActivity {
     String userInputPreviewLower;
     String cutWordSub;
     String completedSentence;
+    String originalWord;
 
     Toast toast;
 
@@ -110,142 +105,213 @@ public class EditingActivity extends AppCompatActivity {
 
     }
 
-    public void goToCodeMakingActivity(View view) {
-        Intent intent = new Intent(EditingActivity.this, CodeMakingActivity.class);
-        startActivity(intent);
-        overridePendingTransition(0, 0);
-    }
+//    public void goToCodeMakingActivity(View view) {
+//        Intent intent = new Intent(EditingActivity.this, CodeMakingActivity.class);
+//        startActivity(intent);
+//        overridePendingTransition(0, 0);
+//    }
+//
+//    public void translatePreview(int seekBarValueInt) {
+//        endPhrase = endPhraseEditText.getText().toString();
+//        userInputPreview = getResources().getString(R.string.preview);
+//
+//
+//
+//
+//        userInputPreviewLower = userInputPreview.toLowerCase();
+//
+//        String[] wordList = userInputPreview.split("[ ]");
+//
+//        ArrayList<String> translatedList = new ArrayList<>();
+//        translatedList.clear();
+////        Log.d("Andres", endPhrase);
+////        Log.d("Andres", seekBarValueInt + "");
+//
+//        for (String word : wordList) {
+//            originalWord = word;
+//            completedSentence = "";
+//            String[] num = identifyUppercase(originalWord).split("(?!^)");
+////            String[] num = identifyUppercase(originalWord).toCharArray();
+//
+//
+//            if (seekBarValueInt != 0) {
+//                word = word.toLowerCase();
+//                try {
+//                    endPhraseBeginning = word.substring(0, seekBarValueInt);
+//                } catch (StringIndexOutOfBoundsException s) {
+//                    endPhraseBeginning = word.substring(0, word.length() - 1);
+//                }
+//                try {
+//                    startingPhrase = word.substring(seekBarValueInt, word.length());
+//                } catch (StringIndexOutOfBoundsException s2) {
+//                    endPhraseBeginning = word.substring(word.length() - 1, word.length());
+//                }
+//
+//                cutWord = startingPhrase + endPhraseBeginning;
+//
+//                cutWordSub = cutWord;
+//                for (int i = 0; i < cutWord.length(); i++) {
+//
+//                    for (String n : num) {
+//
+//                        try {
+//
+//                            if (i == Long.parseLong(n)) {
+//
+//                                try {
+//                                    String character = cutWordSub.charAt((int) Long.parseLong(n)) + "";
+//                                    cutWordSub = cutWordSub.substring(0, (int) Long.parseLong(n)) + character.toUpperCase() + cutWordSub.substring((int) Long.parseLong(n) + 1);
+//                                } catch (StringIndexOutOfBoundsException s) {
+//                                        lettersCutSeekBar.setProgress(lettersCutSeekBar.getProgress() - 1);
+//                                }
+//                            }
+//                        } catch (NumberFormatException NFE) {
+////
+//                        }
+//                    }
+//                }
+//
+//
+//
+//                cutWord = cutWordSub;
+////
+//                int numOfCaps = 0;
+//                for (int i = 0; i < cutWord.length(); i++) {
+//                    if (isUpperCase(cutWord.charAt(i) + "")) {
+//                        numOfCaps += 1;
+//                    }
+//                }
+//                if (numOfCaps > 1) {
+//                    translatedList.add(cutWord + endPhrase.toUpperCase());
+//
+//                } else {
+//                    translatedList.add(cutWord + endPhrase);
+//                }
+//            } else {
+//                int numOfCaps = 0;
+//                for (int i = 0; i < word.length(); i++) {
+//                    if (isUpperCase(word.charAt(i) + "")) {
+//                        numOfCaps += 1;
+//                    }
+//                }
+//                if (numOfCaps > 1) {
+//                    translatedList.add(word + endPhrase.toUpperCase());
+//                } else {
+//                    translatedList.add(word + endPhrase);
+//                }
+//
+//            }
+//
+//            for (String words : translatedList) {
+//                completedSentence += words + " ";
+//            }
+////            completedSentence = addSpaceForEveryWord(completedSentence, translatedList);
+//
+//        }
+//
+//
+//        previewTextView.setText(completedSentence);
+//    }
+//
+//    public String identifyUppercase(String word) {
+//        String capString = "";
+//        for (int i = 0; i < word.length(); i++){
+//            if (isUpperCase(word.charAt(i) + "")) {
+//                capString = capString + i;
+//            }
+//            //Process char
+//        }
+//        return capString;
+//    }
+//
+//    public boolean isUpperCase(String string) {
+//        boolean hasUppercase = !string.equals(string.toLowerCase());
+//        if (!hasUppercase) {
+//            return false;
+//        }
+//        if (hasUppercase) {
+//            return true;
+//        }else{
+//            return false;
+//        }
+//    }
+//
+//
+//    public void showToast(Context context, String message, int duration) {
+//        if (toast != null) {
+//            toast.cancel();
+//        }
+//        toast = Toast.makeText(context, message, duration);
+//        toast.show();
+//    }
+//
+//    public String addSpaceForEveryWord(String sentence, ArrayList<String> transList) {
+//        for (String words : transList) {
+//            sentence = sentence + words + " ";
+//        }
+//        return sentence;
+//    }
+//
+//    public static boolean identifyPunctuationOrNum(String inputString) {
+//
+//        String specialCharacters = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~0123456789";
+//        String[] strlCharactersArray = new String[inputString.length()];
+//        for (int i = 0; i < inputString.length(); i++) {
+//            strlCharactersArray[i] = Character
+//                    .toString(inputString.charAt(i));
+//        }
+//        //now  strlCharactersArray[i]=[A, d, i, t, y, a]
+//        int count = 0;
+//        for (int i = 0; i <  strlCharactersArray.length; i++) {
+//            if (specialCharacters.contains( strlCharactersArray[i])) {
+//                count++;
+//            }
+//
+//        }
+//
+//        if (inputString != null && count == 0) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
 
-    public void translatePreview(int seekBarValueInt) {
+    public void translatePreview(int seekBarValue) {
+        String latin = "";
         endPhrase = endPhraseEditText.getText().toString();
         userInputPreview = getResources().getString(R.string.preview);
-
-        String[] num = identifyUppercase(userInputPreview).split("(?!^)");
-        Log.d("Andres", num + "");
-
-        userInputPreviewLower = userInputPreview.toLowerCase();
-
-        String[] wordList = userInputPreview.split("[ ]");
-
-        ArrayList<String> translatedList = new ArrayList<>();
-        translatedList.clear();
-        Toast.makeText(this, "" + translatedList, Toast.LENGTH_SHORT).show();
-//        Log.d("Andres", endPhrase);
-//        Log.d("Andres", seekBarValueInt + "");
-
-        for (String word : wordList) {
-            if (seekBarValueInt != 0) {
-                try {
-                    endPhraseBeginning = word.substring(0, seekBarValueInt);
-                } catch (StringIndexOutOfBoundsException s) {
-                    showToast(EditingActivity.this, "Nope", 0);
-                }
-                try {
-                    startingPhrase = word.substring(seekBarValueInt, word.length());
-                } catch (StringIndexOutOfBoundsException s2) {
-                    showToast(EditingActivity.this, "Nope2", 0);
-                }
-
-                cutWord = startingPhrase + endPhraseBeginning;
-
-                cutWordSub = cutWord;
-                for (int i = 0; i < cutWord.length(); i++) {
-
-                    for (String n : num) {
-
-                        try {
-
-                            if (i == Long.parseLong(n)) {
-
-                                try {
-                                    String character = cutWordSub.charAt((int) Long.parseLong(n)) + "";
-                                    cutWordSub = cutWordSub.substring(0, (int) Long.parseLong(n)) + character.toUpperCase() + cutWordSub.substring((int) Long.parseLong(n) + 1);
-//                            showToast(EditingActivity.this, cutWordSub, 0);
-                                } catch (StringIndexOutOfBoundsException s) {
-                                    showToast(EditingActivity.this, "Nope3", 1);
-                                }
-                            }
-                        } catch (NumberFormatException NFE) {
-//
-                        }
-                    }
-                }
-
-                cutWord = cutWordSub;
-
-                int numOfCaps = 0;
-                for (int i = 0; i < cutWord.length(); i++) {
-                    if (isUpperCase(cutWord.charAt(i) + "")) {
-                        numOfCaps += 1;
-                    }
-                }
-                if (numOfCaps > 1) {
-                    translatedList.add(cutWord + endPhrase.toUpperCase());
-
-                } else {
-                    translatedList.add(cutWord + endPhrase);
-                }
-            } else {
-                int numOfCaps = 0;
-                for (int i = 0; i < word.length(); i++) {
-                    if (isUpperCase(word.charAt(i) + "")) {
-                        numOfCaps += 1;
-                    }
-                }
-                if (numOfCaps > 1) {
-                    translatedList.add(word + endPhrase.toUpperCase());
-                } else {
-                    translatedList.add(word + endPhrase);
-                }
-
+        int i = 0;
+        while (i < userInputPreview.length()) {
+            while (i < userInputPreview.length() && !isLetter(userInputPreview.charAt(i))) {
+                latin = latin + userInputPreview.charAt(i);
+                i++;
             }
-
-            completedSentence = addSpaceForEveryWord(completedSentence, translatedList);
-
-        }
-
-
-
-        previewTextView.setText(completedSentence.trim());
-    }
-
-    public String identifyUppercase(String word) {
-        String capString = "";
-        for (int i = 0; i < word.length(); i++){
-            if (isUpperCase(word.charAt(i) + "")) {
-                capString = capString + i;
+            if (i >= userInputPreview.length())
+                break;
+            int begin = i;
+            while (i < userInputPreview.length() && isLetter(userInputPreview.charAt(i))) {
+                i++;
             }
-            //Process char
+            int end = i;
+            latin = latin + pigWord(userInputPreview.substring(begin, end));
         }
-        return capString;
+        previewTextView.setText(latin);
     }
 
-    public boolean isUpperCase(String string) {
-        boolean hasUppercase = !string.equals(string.toLowerCase());
-        if (!hasUppercase) {
-            return false;
-        }
-        if (hasUppercase) {
-            return true;
-        }else{
-            return false;
-        }
+    public boolean isLetter(char c) {
+        return ((c >= 'A' && c <= 'z') || (c >= 'a' && c <= 'z'));
     }
 
+    public String pigWord(String word) {
+//        int split = firstVowel(word);
+//        if (split == 0) {
+//            return word.substring(split) + word.substring(0, split) + "way";
+//        } else {
 
-    public void showToast(Context context, String message, int duration) {
-        if (toast != null) {
-            toast.cancel();
-        }
-        toast = Toast.makeText(context, message, duration);
-        toast.show();
+
+            return cutWord + endPhrase;
+//        }
     }
 
-    public String addSpaceForEveryWord(String sentence, ArrayList<String> transList) {
-        for (String words : transList) {
-            sentence = sentence + words + " ";
-        }
-        return sentence;
-    }
 
 }
